af273c09894bb9bd43170798378c49a39f3487d6
Thane <thabay0606@students.maquoketaschools.org>
2026-04-14 14:05:19 -0500

    Maybe.

--- Person.cs
+++ Person.cs
@@ -6,4 +6,18 @@ public class Person{
    public int Age;
 
    //Add your code here
+   public string Person(string FirstName, string LastName, int Age){
+      FirstName = firstName;
+      LastName = lastName;
+      Age = age;
+
+      return $"first name: {firstName}, last name: {lastName}, age: {age}";
+   }
+
+   public string Person(string FirstName, string LastName){
+      this.FirstName = firstName;
+      this.LastName = lastName;
+
+      return $"first name: {firstName}, last name: {lastName}";
+   }
 }
